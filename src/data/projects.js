11a1c12996d9bfb94a7e1a26c70bd3241350f9ad
Kgethego05 <mabilokgethego@gmail.com>
2026-09-15 19:25:39 +0200

    Add project screenshots

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,6 +8,9 @@ import SignInPage from "../assets/images/textToSql/signInPage.png";
 import SignUpPage from "../assets/images/textToSql/signUpPage.png";
 import DashboardPage1 from "../assets/images/textToSql/dashboardPage1.png";
 import DashboardPage2 from "../assets/images/textToSql/dashboardPage2.png";
+import DashboardPage3 from "../assets/images/textToSql/dashboardPage3.png";
+import DashboardPage4 from "../assets/images/textToSql/dashboardPage4.png";
+import DashboardPage5 from "../assets/images/textToSql/dashboardPage5.png"; 
 
 // AI BI Dashboard / Excel Upload
 import excelHomePage from "../assets/images/excelUpload/homePage.png";
@@ -15,6 +18,10 @@ import excelSignInPage from "../assets/images/excelUpload/signInPage.png";
 import excelSignUpPage from "../assets/images/excelUpload/signUpPage.png";
 import excelDashboardPage1 from "../assets/images/excelUpload/dashboardPage1.png";
 import excelDashboardPage2 from "../assets/images/excelUpload/dashboardPage2.png";
+import excelDashboardPage3 from "../assets/images/excelUpload/dashboardPage3.png";
+import excelDashboardPage4 from "../assets/images/excelUpload/dashboardPage4.png";
+import excelDashboardPage5 from "../assets/images/excelUpload/dashboardPage5.png";
+import excelDashboardPage6 from "../assets/images/excelUpload/dashboardPage6.png";
 
 const projects = [
 
@@ -68,7 +75,10 @@ const projects = [
             SignInPage,
             SignUpPage,
             DashboardPage1,
-            DashboardPage2
+            DashboardPage2,
+            DashboardPage3,
+            DashboardPage4,
+            DashboardPage5
         ],
 
 
@@ -114,7 +124,11 @@ const projects = [
             excelSignInPage,
             excelSignUpPage,
             excelDashboardPage1,
-            excelDashboardPage2
+            excelDashboardPage2,
+            excelDashboardPage3,
+            excelDashboardPage4,
+            excelDashboardPage5,
+            excelDashboardPage6
         ],
 
 
